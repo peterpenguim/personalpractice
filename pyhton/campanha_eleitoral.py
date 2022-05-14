92206dc84cd5dd50db2baca478125bea3d3e51fa
Peter P <104590406+peterpenguim@users.noreply.github.com>
2022-05-13 22:44:50 -0300

    Add files via upload

---
+++ pyhton/campanha_eleitoral.py
@@ -0,0 +1,33 @@
+#THE HUXLEY 931
+valores = []
+candidatos = {}
+while True:
+    candidato = input()
+    if candidato == "FIM":
+        break
+    else:
+        candidato = candidato.split()
+        for i in range(int(candidato[-1])):
+            anuncio = input()
+            if anuncio == "internet":
+                valores.append(3000)
+            elif anuncio == "revista":
+                valores.append(750)
+            elif anuncio == "outdoor":
+                valores.append(1500)
+            elif anuncio == "radio":
+                radio = input()
+                if radio == "fm":
+                    valores.append(500)
+                elif radio == "am":
+                    valores.append(300)
+            elif anuncio == "tv":
+                hora = int(input())
+                if hora <= 20:
+                    valores.append(1200)
+                elif hora > 20:
+                    valores.append(2000)
+        candidatos[candidato[0]] = sum(valores)
+        valores.clear()
+for chave, valor in candidatos.items():
+    print(f"{chave}: {valor:.2f}")
