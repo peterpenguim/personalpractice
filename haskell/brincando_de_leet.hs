6120df895679be938d59579dc0c467f2d7688985
Peter P <104590406+peterpenguim@users.noreply.github.com>
2022-05-21 18:20:51 -0300

    Update brincando_de_leet.hs

--- haskell/brincando_de_leet.hs
+++ haskell/brincando_de_leet.hs
@@ -3,9 +3,9 @@ main = do
  entrada' <- getLine
  let entrada = entrada'
  let soma = (contador entrada 'a' + contador entrada 'A' + contador entrada 'e' + contador entrada 'E' + contador entrada 'o' + contador entrada 'O' + contador entrada 'i' + contador entrada 'I' + contador entrada 't' + contador entrada 'T' + contador entrada 's' + contador entrada 'S')
- if not(null entrada) && (encontrarElem '0' entrada || encontrarElem '1' entrada || encontrarElem '2' entrada || encontrarElem '3' entrada || encontrarElem '4' entrada || encontrarElem '5' entrada || encontrarElem '6' entrada || encontrarElem '7' entrada || encontrarElem '8' entrada || encontrarElem '9' entrada) 
+ if not(null entrada) && (elem '0' entrada || elem '1' entrada || elem '2' entrada || elem '3' entrada || elem '4' entrada || elem '5' entrada || elem '6' entrada || elem '7' entrada || elem '8' entrada || elem '9' entrada)
    then putStrLn $ "numeros\n" ++ show 0
- else if not(null entrada) && not(encontrarElem '0' entrada || encontrarElem '1' entrada || encontrarElem '2' entrada || encontrarElem '3' entrada || encontrarElem '4' entrada || encontrarElem '5' entrada || encontrarElem '6' entrada || encontrarElem '7' entrada || encontrarElem '8' entrada || encontrarElem '9' entrada)
+ else if not(null entrada) && not(elem '0' entrada || elem '1' entrada || elem '2' entrada || elem '3' entrada || elem '4' entrada || elem '5' entrada || elem '6' entrada || elem '7' entrada || elem '8' entrada || elem '9' entrada)
    then putStrLn $ reverse (mudarLetra entrada) ++ "\n" ++ show soma
  else putStrLn $ "vazio\n" ++ show 0
 
@@ -27,8 +27,3 @@ mudarLetra (x : xs)
 
 contador :: String -> Char -> Int
 contador str c = length $ filter (== c) str
-
-encontrarElem _ [] = False
-encontrarElem n (x:xs)
-  | x == n = True
-  | otherwise = encontrarElem n xs
